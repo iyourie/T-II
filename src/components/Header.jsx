5ff5038815b711199fdb213294b4eb0a6580ba88
iyourie <azhiedif@gmail.com>
2022-09-28 15:24:38 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
+import image from '../img/favicon.ico'
 
 function Header() {
+    const [id, setId] = useState('')
     return (
         <header>
             <div className="container">
-
-            </div>
-        </header>
+                <img src={image} alt="" />
+                <ul>
+                    <li><a href={id} onClick={() => { setId('#articles') }}>Articles</a></li>
+                    <li><a href={id} onClick={() => { setId('#gallery') }}>Gallery</a></li>
+                    <li><a href={id} onClick={() => { setId('#features') }}>Features</a></li>
+                    <li><a href={id} onClick={() => { setId('#otherLinks') }}>Other Links</a></li>
+                </ul>
+            </div >
+        </header >
     )
 }
 export default Header
