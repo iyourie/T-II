7894c11cbef770e6c1969a3042c5282ff616506e
iyourie <azhiedif@gmail.com>
2022-09-28 15:04:34 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react'
 import image from '../img/favicon.ico'
 
 function Header() {
+    const [id, setId] = useState('')
     return (
         <header>
             <div className="container">
                 <img src={image} alt="" />
                 <ul>
-                    <li>home</li>
-                    <li>services</li>
-                    <li>portfolio</li>
-                    <li>about</li>
-                    <li>pricing</li>
-                    <li>contact</li>
+                    <li><a href={id} onClick={() => setId('#home')}> home</a></li>
+                    <li><a href={id} onClick={() => setId('#services')}>services</a> </li>
+                    <li><a href={id} onClick={() => setId('#portfolio')}>portfolio</a></li>
+                    <li><a href={id} onClick={() => setId('#about')}>about</a></li>
+                    <li><a href={id} onClick={() => setId('#pricing')}>pricing</a></li>
+                    <li><a href={id} onClick={() => setId('#contact')}>contact</a></li>
                     <li><i className="fa-solid fa-magnifying-glass"></i></li>
                 </ul>
             </div>
